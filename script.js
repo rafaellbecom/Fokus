d7e211638f86c3332d99375aafabd0e62f2927a1
Rafael Lima Barreto <rafaellbecom@gmail.com>
2024-02-14 21:40:25 -0300

    Ajustando tempo do cronômetro

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const audioPlay = new Audio('/sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
-let tempoDecorridoEmSegundos = 30
+let tempoDecorridoEmSegundos = 1800
 let intervaloId = null
 
 musica.loop = true
